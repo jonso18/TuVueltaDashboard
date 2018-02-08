6eed6dfa0d4679ef2fcadd94afe8bff848d276b5
ronaldomb98 <ronaldomb98@gmail.com>
2018-02-08 18:52:22 -0500

    Added builform function

--- src/app/solicitudes/solicitud-form/solicitud-form.component.ts
+++ src/app/solicitudes/solicitud-form/solicitud-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-solicitud-form',
@@ -7,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
-    
+    this.buildForm();
   }
 
+  buildForm(){
+    this.form = this.formBuilder.group({
+      puntoInicio: [null, [Validators.required]],
+	    puntoFinal: [null, [Validators.required]],
+	    esPagoConTarjeta: [null, [Validators.required]],
+	    Nombres: [null, [Validators.required]],
+	    Apellidos: [null, [Validators.required]],
+	    Telefono: [null, [Validators.required]],
+	    Celular: [null, [Validators.required]],
+	    ValorDomicilio: [null, [Validators.required]],
+	    codigoCiudad: [null, [Validators.required]],
+	    Descripcion: [null, [Validators.required]],
+	    DescripcionDomicilio: [null, [Validators.required]],
+    })
+  }
 }
