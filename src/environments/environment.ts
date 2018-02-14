90a82d338e26700a8bf931224531a50fd5508597
ronaldomb98 <ronaldomb98@gmail.com>
2018-02-14 11:25:05 -0500

    Added rules ptuvuelta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,15 +6,15 @@
 export const environment = {
   production: false,
   firebase:{
-   apiKey: "AIzaSyB_Shc3PWqhPr2JCC_GTCheczHMyyttNmY",
-   authDomain: "tuvueltap.firebaseapp.com",
-   databaseURL: "https://tuvueltap.firebaseio.com",
-    projectId: "tuvueltap",
-    storageBucket: "tuvueltap.appspot.com",
-    messagingSenderId: "565294870666"
+   apiKey: "AIzaSyBUS5cy6FLocCnKclySb-6zMb9_5lTWpA8",
+    authDomain: "ptuvuelta.firebaseapp.com",
+    databaseURL: "https://ptuvuelta.firebaseio.com",
+    projectId: "ptuvuelta",
+    storageBucket: "ptuvuelta.appspot.com",
+    messagingSenderId: "1008156158697"
   },
 
   baseapi: {
-    tuvuelta: 'https://us-central1-tuvuelta-produccion.cloudfunctions.net/asdfas'
+    tuvuelta: 'https://us-central1-ptuvuelta.cloudfunctions.net/asdfas'
   }
 };
